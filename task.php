c1607af9bdfc08d8d84c4d27d91dfb24543a6f56
Josh <joshua222_98@yahoo.com>
2021-01-20 19:20:10 +0800

    task design

--- task.php
+++ task.php
@@ -5,25 +5,27 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Task</title>
+  <style>
+    <?php include 'styles/task.css'; ?>
+  </style>
 </head>
 
 <body>
   <main id="main_task">
     <section id="inbox_container">
-      <header>
-        <h1>Inbox</h1>
-      </header>
       <form action="" method="POST" id="task_form">
+        <h1 id="task_title">ToDo</h1>
         <div id="link_container">
           <div id="task_link_container">
             <a href="#" id="task_link">Task</a>
+            <div id="task_underline"></div>
           </div>
           <div id="done_link_container">
             <a href="#" id="done_link">Done</a>
           </div>
         </div>
 
-        <div>
+        <div id="task_group">
           <input type="text" name="txt_task" id="txt_task" placeholder="Add new item" />
           <input type="submit" name="btn_add_task" id="btn_add_task" value="+" />
         </div>
